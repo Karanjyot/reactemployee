b45279412e0e5cd40c48cffb9db37bd75df355c1
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-03-26 19:55:15 -0400

    Updates

---
+++ precache-manifest.edffe38947861fda87bdacac8fe7358a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0562c99aaa16c61bd0b24576bbc9aacb",
+    "url": "/reactemployee/index.html"
+  },
+  {
+    "revision": "407cae694833687ae6ae",
+    "url": "/reactemployee/static/js/2.67a2b1ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactemployee/static/js/2.67a2b1ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05438379d786bd63fad",
+    "url": "/reactemployee/static/js/main.5a287fb1.chunk.js"
+  },
+  {
+    "revision": "7d90d8a21d0cc34a98e6",
+    "url": "/reactemployee/static/js/runtime-main.77444709.js"
+  }
+]);
